544b80eaf2c758a2c54ccab454eb6224727414e7
kimminkyu <kmk50009@naver.com>
2022-01-31 00:44:12 +0900

    React.memo(), useCallback()

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import React from 'react';
-
-import './App.css';
+import React, { useState, useCallback } from "react";
+import "./App.css";
+import Button from "./components/UI/Button/Button";
+import DemoOutput from "./components/Demo/DemoOutput";
 
 function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
+
+  console.log("App Running");
+
+  const toggleParagraphHandler = useCallback(() => {
+    if (allowToggle) {
+      setShowParagraph((preShowParagraph) => !preShowParagraph);
+    }
+  }, [allowToggle]);
+
+  const allowToggleHandler = () => {
+    setAllowToggle(true);
+  };
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      <DemoOutput show={showParagraph} />
+      <Button onClick={allowToggleHandler}>Allow Toggle!</Button>
+      <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
     </div>
   );
 }
---
+++ src/components/Demo/MyParagraph.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const MyParagraph = props => {
+    console.log('MyParagraph Running');
+    
+    return <p>{props.children}</p>
+}
+
+export default MyParagraph;
